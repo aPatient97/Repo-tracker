55157b2ca86fd26647e03b8f37b247bec6ddb6fc
Jaxsan Sivanesan <jaxsan.sivanesan@gmail.com>
2022-04-27 17:01:06 +0100

    Changed the FetchGitHub Component

--- repo-tracker/src/components/FetchGithub/index.js
+++ repo-tracker/src/components/FetchGithub/index.js
@@ -1,17 +1,15 @@
 import React, { useEffect, useState } from 'react'
 
 function FetchGithub({username}) {
-  const [repos, setRepos] = useState([])
-  console.log('Outer function called')
-  // console.log(username)
+  const [username, setUsername] = useState(username)
+  const [data, setData] = useState([])
   useEffect(()=>{
     const fetchGithub = async (username) => {
       try {
           console.log('function called')
-          // console.log(username)
           const {data} = await fetch(`https://api.github.com/users/${username}/repos`)
-          // console.log(data)
-          setRepos(data)
+          console.log(data)
+          setData(data)
       } catch (error) {
           console.warn(error)
       }
